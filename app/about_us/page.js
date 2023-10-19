fb2e61a3c9215ae64df7717eeb233f2454dc3656
Varsani2520 <varsaniranjani@gmail.com>
2023-10-19 11:47:57 +0530

    about

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -19,18 +19,19 @@ export default function About() {
       <div className=" top-0 left-0 mt-16 justify-center items-center flex bg-white dark:bg-gray-900">
         <div className="max-w-screen-xl  items-center justify-between  p-4">
           {about.map((response) => (
-            <>
-            
+            <div key={response.id}>
               <div className=" w-full opacity-[0.5]">
-                <img src={response.src} className='w-[1500px]'/>
+                <img src={response.src} className="w-[1500px]" />
               </div>
-              <div class="grid grid-cols-2 gap-2 mt-[-0px] relative ml-[10px] ">
+              <div className="grid grid-cols-2 gap-2 mt-[-0px] relative ml-[10px] ">
                 <div>
-                  <img src={response.image} />
+                  <img src={response.image} alt="hi"></img>
                 </div>
 
-                
-                <div>{response.name}{response.skills}</div>
+                <div>
+                  {response.name}
+                  {response.skills}
+                </div>
               </div>
               {/* description */}
               <div className="text-gray-600  font-serif text-2xl">
@@ -41,7 +42,7 @@ export default function About() {
                 <div>{response.desc3}</div>
                 <br />
               </div>
-            </>
+            </div>
           ))}
         </div>
       </div>
--- app/api/about_us/route.js
+++ app/api/about_us/route.js
@@ -3,19 +3,25 @@ import { NextResponse } from "next/server";
 export function POST(request) {
   const about_us = [
     {
+      id: 1,
       src: "https://cdn-iemok.nitrocdn.com/shKFQIhqmtPcYKyAdNbDrBqdXdAOsudW/assets/images/optimized/rev-6b308ac/wp-content/uploads/2014/11/banner_1.jpg",
     },
     {
-      image: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+      id: 2,
+      image:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
       skills: "Creative, Whimsical, Philosophical & Practical",
     },
-    
-      {
-        desc1: "Sanchi’s relentless enthusiasm to push boundaries drives her to question, learn and grow. She is involved in projects from conceptualization to completion. Her innate sense of style, ability to blend in functionality with materials &aesthetics and whimsical imagination is her USP.",
-        desc2: "Having trained under reputed architectural firm in (Ahmedabad) India. Sanchi, then ventured out to work independently and has more than 8 years of designing experienceand now has established her own firm with team of experts to fulfill all kinds of design requirements at one place, for its clients, making their lives more easy and beautiful.",
-        desc3: "There is a lot to say about the firm and its work, but we would suggest checking our work portfolio, as we believe what they have said, “work speaks more than words”.",
-      }
 
+    {
+      id: 3,
+      desc1:
+        "Sanchi’s relentless enthusiasm to push boundaries drives her to question, learn and grow. She is involved in projects from conceptualization to completion. Her innate sense of style, ability to blend in functionality with materials &aesthetics and whimsical imagination is her USP.",
+      desc2:
+        "Having trained under reputed architectural firm in (Ahmedabad) India. Sanchi, then ventured out to work independently and has more than 8 years of designing experienceand now has established her own firm with team of experts to fulfill all kinds of design requirements at one place, for its clients, making their lives more easy and beautiful.",
+      desc3:
+        "There is a lot to say about the firm and its work, but we would suggest checking our work portfolio, as we believe what they have said, “work speaks more than words”.",
+    },
   ];
   return NextResponse.json(about_us);
 }
