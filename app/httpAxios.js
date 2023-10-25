4258a58c3cafade22c858964ca249b10e8e0f4bd
v_a_r_s_a_n_i <137641079+Varsani2520@users.noreply.github.com>
2023-10-25 23:34:41 +0530

    Update httpAxios.js

--- app/httpAxios.js
+++ app/httpAxios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // helps to use api with baseurl
 export const httpAxios = axios.create({
-  baseURL: "https://p-dqmu.vercel.app/",
+  baseURL: "https://p-k0wyhr1ui-varsani-ranjanis-projects.vercel.app/",
 });
