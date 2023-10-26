37f369c3227e5a9edd94346cf2779de46f13488c
Varsani2520 <varsaniranjani@gmail.com>
2023-10-26 11:28:41 +0530

    hiii

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -4,25 +4,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeToCartItem } from "../action/action";
 
 const page = () => {
-  // who cpy here ??
+  
   const carts = useSelector((state) => state.cart.cartItems);
   const dispatch = useDispatch();
   function rmv(item) {
     
   }
   return (
-    <div className="mt-7">
-      {/* ehat about map */}
-      {carts.map((cart) => {
-        return (
-          <div className="mt-10">
+    <div className="mt-20">
+       {carts.length === 0 ? (
+        <img src="https://w7.pngwing.com/pngs/277/965/png-transparent-empty-cart-illustration-thumbnail.png" alt="empty cart img"/>
+      ) : (
+      carts.map((cart) => (
+        
+          <div className="mt-10" key={cart.id}>
             <h1>{cart.title}</h1>
-            <img src={cart.image} />
+            <img src={cart.image} alt={cart.title}/>
             {/* rmv=remove */}
             <button onClick={() => dispatch(removeToCartItem(cart))}>remove</button>
           </div>
-        );
-      })}
+        )
+      ))}
     </div>
   );
 };
